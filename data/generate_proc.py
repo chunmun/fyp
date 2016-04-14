2c5ddf567342249caf49cdd9576b63a87129a65e
Chunmun <xilasirc@gmail.com>
2016-04-14 10:04:49 +0800

    Add data files

---
+++ data/generate_proc.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f6604b160fa82b162804d64eecb46a712cbf0a30706dd02948ccad6a72b765be
+size 5689
---
+++ data/preproc-blank.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c1093ceca6554c1c49ff77aea1d5136ba9c1e739a76065d975999611954fad93
+size 4858
